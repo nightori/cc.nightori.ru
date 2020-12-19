cc50ff332f6b64a9146fad92ae5b0a44ca12408e
Sergey Savin <admin@nightori.ru>
2020-12-19 19:34:57 +0600

    Added web layer tests

---
+++ src/test/java/ru/nightori/cc/WebLayerTests.java
@@ -0,0 +1,67 @@
+package ru.nightori.cc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.nightori.cc.config.Config.APP_DOMAIN;
+
+@WebMvcTest(RedirectController.class)
+class WebLayerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    RedirectService mockedClientService;
+
+    @MockBean
+    ClientCacheService mockedCacheService;
+
+    @Test
+    void homeRedirectTest() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/home"));
+    }
+
+    @Test
+    void createRedirectTest() throws Exception {
+        String expected = String.format("https://%s/null", APP_DOMAIN);
+
+        mockMvc.perform(post("/api")
+                .param("shortUrl", "google")
+                .param("destination", "https://google.com")
+                .param("password", "12345"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(expected));
+        verify(mockedClientService).createRedirect(any(), any(), any());
+    }
+
+    @Test
+    void deleteRedirectTest() throws Exception {
+        mockMvc.perform(delete("/api")
+                .param("shortUrl", "google")
+                .param("password", "12345"))
+                .andExpect(status().isOk());
+        verify(mockedClientService).deleteRedirect(any(), any());
+    }
+
+    @Test
+    void urlRedirectTest() throws Exception {
+        String expectedURL = "https://example.com";
+        when(mockedClientService.getRedirectUrl(anyString())).thenReturn(expectedURL);
+
+        mockMvc.perform(get("/randomUrl"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", expectedURL));
+    }
+}
